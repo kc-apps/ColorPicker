740ab779b6d630d04c6c27c35196608db550421d
KC Apps <keishi.okazaki.apps@gmail.com>
2020-12-18 12:04:58 +0900

    Initial Commit

---
+++ ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectName  : ColorPicker
+//  File Name    : ViewController.swift
+//  Created Date : 2020/12/18
+//  
+//  Copyright © 2019-2020 KC Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
