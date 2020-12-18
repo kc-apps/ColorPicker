dae5928538475e239cbe55dfdbf978fb7ee098e9
KC Apps <keishi.okazaki.apps@gmail.com>
2020-12-18 19:56:44 +0900

    1.0

--- ColorPicker/Source/Controller/ViewController.swift
+++ ColorPicker/Source/Controller/ViewController.swift
@@ -10,12 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 	@IBOutlet fileprivate var viewColor: UIView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
-
 }
 
 // MARK: - Button
